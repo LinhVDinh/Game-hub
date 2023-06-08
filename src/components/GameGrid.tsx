503623a857259b93344ea5d65c644c031dd0c25b
Linh <linhvdinh1@gmail.com>
2023-06-08 00:12:53 -0700

    refactored duplicated styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -16,8 +17,16 @@ const GameGrid = () => {
         spacing={10}
       >
         {isLoading
-          ? Skeleton.map((skeleton) => <GameCardSkeleton key={skeleton}/>)
-          : games.map((game) => <GameCard key={game.id} game={game} />)}
+          ? Skeleton.map((skeleton) => (
+              <GameCardContainer>
+                <GameCardSkeleton key={skeleton} />
+              </GameCardContainer>
+            ))
+          : games.map((game) => (
+              <GameCardContainer>
+                <GameCard key={game.id} game={game} />
+              </GameCardContainer>
+            ))}
       </SimpleGrid>
     </>
   );
