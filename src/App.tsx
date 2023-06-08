2037b7f836728179e44a207607bd64d34d618b8f
Linh <linhvdinh1@gmail.com>
2023-06-07 21:07:47 -0700

    created custom hook to fetch games

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`
-    }}>
-      <GridItem area="nav" bg="coral">
-        Nav
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `'nav nav' 'aside main'`
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
-      <Show above="lg"><GridItem area="aside" bg="gold">
-        aside
-      </GridItem></Show>
-      
-      <GridItem area="main" bg="dodgerblue">
-        main
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+
+      <GridItem area="main">
+        <GameGrid />
       </GridItem>
     </Grid>
   );
